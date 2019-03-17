c4ad7896878258164211ac7a350bd8ab1c683c95
WENZHENGHUANG\Wenzheng Huang <zannezankyo@gmail.com>
2019-03-17 00:07:14 -0500

    update environment config

---
+++ LeetCodePreInclude.h
@@ -0,0 +1,11 @@
+#include <vector>
+
+
+using namespace std;
+
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
