2e3d1e362fe8cc03074d75b433736aadc206a04e
WENZHENGHUANG\Wenzheng Huang <zannezankyo@gmail.com>
2019-03-17 21:20:48 -0500

    53 accepted

---
+++ 53.maximum-subarray.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=53 lang=cpp
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (42.97%)
+ * Total Accepted:    478.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * 
+ * 
+ * Follow up:
+ * 
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ * 
+ */
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> sums;
+        sums.resize(nums.size() + 1);
+        sums[0] = 0;
+        for(int i = 0, sum = 0; i < nums.size(); ++i){
+            sum += nums[i];
+            sums[i + 1] = sum;
+            //cout << sum << ",";
+        }
+        int max = 0x80000000;
+        for(int i = 0; i < sums.size(); ++i){
+            for(int j = i + 1; j < sums.size(); ++j){
+                int diff = sums[j] - sums[i];
+                if(diff > max){
+                    max = diff;
+                }
+            }
+        }
+        return max;
+    }
+};
+
