4a1e9c95245460be32698445caa115b718afbe5c
WENZHENGHUANG\Wenzheng Huang <zannezankyo@gmail.com>
2019-06-13 23:05:11 -0500

    5 runtime 82.73%

--- 5.longest-palindromic-substring.cpp
+++ 5.longest-palindromic-substring.cpp
@@ -30,6 +30,106 @@
  * 
  * 
  */
+
+/*
+
+Test Case:
+
+"babad"
+"cbbd"
+"aaaaaaaaaa"
+""
+"cccccccca"
+"ccccccccca"
+"abcda"
+
+*/
+
+#define TIME 1
+#define SPACE 2
+#define BIAS TIME
+
+#if BIAS == TIME
+
+class Solution {
+public:
+
+    int getMaxPalindromeEven(string const &s, int leftStartIndex){
+        int maxLength = 0;
+        int left = leftStartIndex;
+        int right = leftStartIndex + 1;
+        while(left >= 0 && right < s.size()){
+            if(s[left] != s[right]){
+                return maxLength;
+            }
+            --left;
+            ++right;
+            maxLength += 2;
+        }
+        return maxLength;
+    }
+
+    int getMaxPalindromeOdd(string const &s, int centerIndex){
+        int maxLength = 1;
+        int left = centerIndex - maxLength;
+        int right = centerIndex + maxLength;
+        while(left >= 0 && right < s.size()){
+            if(s[left] != s[right]){
+                return maxLength;
+            }
+            --left;
+            ++right;
+            maxLength += 2;
+        }
+        return maxLength;
+    }
+    
+    string longestPalindrome(string s) {
+        if(s.size() <= 1) return s;
+
+        int maxOddLength = 1;
+        int maxOddIndex = 0;
+
+        int maxEvenLength = 0;
+        int maxEvenIndex = 0;
+
+        for(int i = 0; i < s.size(); ++i){
+            if(i+2 < s.size() && s[i] == s[i+2]){
+                int palindromeLength = getMaxPalindromeOdd(s, i+1);
+                if(palindromeLength > maxOddLength){
+                    maxOddLength = palindromeLength;
+                    maxOddIndex = i + 1;
+                }
+            }
+            if(i+1 < s.size() && s[i] == s[i+1]){
+                int palindromeLength = getMaxPalindromeEven(s, i);
+                if(palindromeLength > maxEvenLength){
+                    maxEvenLength = palindromeLength;
+                    maxEvenIndex = i;
+                }
+            }
+        }
+
+        //cout<<maxOddIndex<<':'<<maxOddLength<<','<<maxEvenIndex<<':'<<maxEvenLength<<'|';
+
+        if(maxOddLength > maxEvenLength){
+            return s.substr(maxOddIndex - maxOddLength/2, maxOddLength);
+        }
+        else{
+            return s.substr(maxEvenIndex - maxEvenLength/2 + 1, maxEvenLength);
+        }
+
+        
+
+    }
+
+};
+
+#endif
+
+
+#if BIAS == SPACE
+
 class Solution {
 public:
     bool isPalindrome2(const string& s)
@@ -101,3 +201,5 @@ public:
     }
 };
 
+
+#endif
