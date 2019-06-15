e23bbd610e662edb4f2c2b1158f449c656c79095
WENZHENGHUANG\Wenzheng Huang <zannezankyo@gmail.com>
2019-06-15 00:17:08 -0500

    23 runtime 21%

--- 23.merge-k-sorted-lists.cpp
+++ 23.merge-k-sorted-lists.cpp
@@ -35,6 +35,87 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+
+
+#if 1 //21%
+
+struct ListListNode{
+    ListNode* node;
+    ListListNode* next;
+    ListListNode(ListNode* n) : node(n), next(nullptr){}
+};
+
+class Solution {
+public:
+
+    ListListNode* insertSortedList(ListListNode* head, ListListNode* newList){
+
+        if(newList == nullptr || newList->node == nullptr){
+            return head;
+        }
+
+        if(head == nullptr){
+            return newList;
+        }
+
+        ListListNode* prev = nullptr;
+        ListListNode* current = head;
+        while(current){
+            if(current->node->val >= newList->node->val){
+                if(prev == nullptr){
+                    newList->next = head;
+                    head = newList;
+                }else{
+                    prev->next = newList;
+                    newList->next = current;
+                }
+                return head;
+            }
+            prev = current;
+            current = current->next;
+        }
+        if(prev == nullptr){
+            return newList;
+        }
+        else{
+            prev->next = newList;
+        }
+        return head;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        ListListNode* sortedLists = nullptr;
+        for(ListNode* node : lists){
+            sortedLists = insertSortedList(sortedLists, new ListListNode(node));
+        }
+        ListNode* result = nullptr;
+        ListNode* current = result;
+        while(sortedLists){
+            ListListNode* top = sortedLists;
+            ListListNode* others = sortedLists->next;
+            ListNode* newNode = top->node;
+            if(result == nullptr){
+                result = newNode;
+                current = result;
+            }
+            else{
+                current->next = newNode;
+                current = newNode;
+            }
+            top->node = top->node->next;
+            top->next = nullptr;
+            sortedLists = insertSortedList(others, top);
+            //cout<<"<-"<<sortedLists->node->val;
+        }
+        return result;
+    }
+};
+
+#endif
+
+#if 0 //7%
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
@@ -84,4 +165,5 @@ public:
         return newHead;
     }
 };
+#endif
 
