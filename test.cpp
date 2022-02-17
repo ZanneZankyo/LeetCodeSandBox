876b9c3abf565b7578c93513c1a4d6888a045000
ZanneZankyo <zannezankyo@gmail.com>
2022-02-17 10:50:08 -0600

    Add test.cpp to debug locally

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include "LeetCodePreInclude.h"
+
+Solution solution;
+
+// to run test.cpp, make sure you install C++ extension and run Visual Studio Code from a Developer Command Prompt
+
+void main()
+{
+}
