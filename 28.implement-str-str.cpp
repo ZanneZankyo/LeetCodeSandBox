505cebdd3d82fa58164c68e6e6c0fe4d00110867
ZanneZankyo <zannezankyo@gmail.com>
2022-02-20 07:00:23 -0600

    28 KMT algrorithm

--- 28.implement-str-str.cpp
+++ 28.implement-str-str.cpp
@@ -39,6 +39,57 @@
  * string. This is consistent to C's strstr() and Java's indexOf().
  * 
  */
+#if 1 
+// https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+class Solution {
+private:
+    void KMTPreProcess(vector<int> & outLps, string const & needle){
+        int longestPrefixSurfix = 0;
+        outLps.resize(needle.size(), 0);
+        for(int i = 1; i < needle.size();){
+            if(needle[i] == needle[longestPrefixSurfix]){
+                ++longestPrefixSurfix;
+                outLps[i] = longestPrefixSurfix;
+                ++i;
+            }
+            else if(longestPrefixSurfix){
+                longestPrefixSurfix = outLps[longestPrefixSurfix - 1];
+            }
+            else{
+                outLps[i] = 0;
+                ++i;
+            }
+        }
+    }
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+            return 0;
+        if(haystack.empty())
+            return -1;
+        if(haystack.size() < needle.size())
+            return -1;
+        int i,j;
+        bool match;
+        vector<int> lps;
+        KMTPreProcess(lps, needle);
+        for(i = 0, j = 0; i < haystack.size();){
+            if(haystack[i] == needle[j]){
+                ++j;
+                ++i;
+            }
+            if(j == needle.size()){
+                return i - j;
+            }
+            if(i < haystack.size() && haystack[i] != needle[j]){
+                j ? j = lps[j-1] : ++i;
+            }
+        }
+        return -1;
+    }
+};
+#endif
+#if 0 // brute force time limit excceeded
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -48,9 +99,11 @@ public:
             return -1;
         if(haystack.size() < needle.size())
             return -1;
-        for(int i = 0; i < haystack.size() - needle.size() + 1; i++){
-            bool match = true;
-            for(int j = 0; j < needle.size(); j++){
+        int i,j;
+        bool match;
+        for(i = 0; i < haystack.size() - needle.size() + 1; ++i){
+            match = true;
+            for(j = 0; j < needle.size(); ++j){
                 if(haystack[i + j] != needle[j]){
                     match = false;
                     break;
@@ -62,4 +115,5 @@ public:
         return -1;
     }
 };
+#endif
 
