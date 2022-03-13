2c1a15029097cd48e865c2607123330c9a0d9818
ZanneZankyo <zannezankyo@gmail.com>
2022-03-13 14:21:43 -0500

    94 accepted 58% time 42% mem

---
+++ 94.binary-tree-inorder-traversal.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=94 lang=cpp
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (70.01%)
+ * Likes:    7148
+ * Dislikes: 317
+ * Total Accepted:    1.4M
+ * Total Submissions: 2M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the inorder traversal of its nodes'
+ * values.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traversal(TreeNode* node){
+        if(node->left != nullptr){
+            traversal(node->left);
+        }
+        result.push_back(node->val);
+        if(node->right != nullptr){
+            traversal(node->right);
+        }
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        result.clear();
+        if(root != nullptr){
+            traversal(root);
+        }
+        return result;
+    }
+private:
+    vector<int> result;
+};
+// @lc code=end
+
