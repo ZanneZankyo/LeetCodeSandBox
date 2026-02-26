d163530ed8e9b45b971e2e93eeec5aef61dfd254
ZanneZankyo <zannezankyo@gmail.com>
2026-02-26 12:18:44 -0600

    240 accepted 88% time 67% mem

--- 240.search-a-2-d-matrix-ii.cpp
+++ 240.search-a-2-d-matrix-ii.cpp
@@ -38,8 +38,31 @@
  * Given target = 20, return false.
  * 
  */
+#include <stack>
+#include <unordered_set>
+using namespace std;
 class Solution {
 public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty() || matrix[0].empty()) {
+            return false;
+        }
+        int i = 0;
+        int j = matrix[0].size() - 1;
+        while(i < matrix.size() && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            else if (matrix[i][j] > target) {
+                --j;
+            }
+            else {
+                ++i;
+            }
+        }
+        return false;
+    }
+#if 0 // 19% time
     int binarySearch(vector<int>& nums ,int startIndex, int endIndex, int target){
         int near = startIndex;
         int far = endIndex;
@@ -78,5 +101,5 @@ public:
         }
         return false;
     }
+#endif
 };
-
