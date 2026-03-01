e380446d829afa8944de36630b71bfe2a1a4db48
ZanneZankyo <zannezankyo@gmail.com>
2026-03-01 11:49:21 -0600

    108 accepted

---
+++ 108.convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=108 lang=cpp
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ *
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (75.19%)
+ * Likes:    11865
+ * Dislikes: 640
+ * Total Accepted:    1.7M
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * Given an integer array nums where the elements are sorted in ascending
+ * order, convert it to a height-balanced binary search tree.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [-10,-3,0,5,9]
+ * Output: [0,-3,9,-10,null,5]
+ * Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,3]
+ * Output: [3,1]
+ * Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums is sorted in a strictly increasing order.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* step(int i, int j) {
+        if (i > j) {
+            return nullptr;
+        }
+        int mid = (i+j)/2;
+        TreeNode *node = new TreeNode(nums->at(mid));
+        node->left = step(i, mid-1);
+        node->right = step(mid+1, j);
+        return node;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& _nums) {
+        nums = &_nums;
+        return step(0,nums->size()-1);
+    }
+private:
+    vector<int>* nums;
+};
+
+// @lc code=end
+
